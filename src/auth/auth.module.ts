74ac6717d06f6ff0172632914c17fa99b6e969c5
Adrian Camarena <adriancamarena2310mail.com>
2023-12-03 12:21:20 -0600

    video-343 DTO - Data Transfer Object

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,7 +3,6 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './entities/user.entity';
-
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
