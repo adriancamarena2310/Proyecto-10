9a39e0963e7b02043f84906f2bddcd34f6f68a13
Adrian Camarena <adriancamarena2310mail.com>
2023-12-03 11:55:23 -0600

    video-341 Variables de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     AuthModule,
-    MongooseModule.forRoot('mongodb://localhost/27017'),
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    ConfigModule.forRoot(),
   ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+
+}
